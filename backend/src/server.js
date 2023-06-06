e82f0ec73501d4e8d033488ea475084b887fca76
Arabi Kabir <mail2arabi@gmail.com>
2023-06-06 10:46:28 +0600

    path added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const path = require('path')
 
-require('dotenv').config({ path: path.join(__dirname, ".env") })
+require('dotenv').config({ path: path.join(__dirname, '..', ".env") })
 
 const app = require('./app')
 const { mongoConnect } = require('../src/services/mongo')
