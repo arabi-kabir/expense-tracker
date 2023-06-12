4a4f5af340d3f77938b7a0178551430b756e8dc8
Arabi Kabir <mail2arabi@gmail.com>
2023-06-12 10:32:33 +0600

    upload file path changed to build 2

--- backend/src/services/file-upload.js
+++ backend/src/services/file-upload.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid')
 const path = require('path')
 
 const storage = multer.diskStorage({
-    destination: '../frontend/build/uploads/',
+    destination: '../../frontend/build/uploads/',
     filename: function(req, file, cb) {
         cb(null, uuidv4() + '-' + Date.now() + path.extname(file.originalname))
     }
